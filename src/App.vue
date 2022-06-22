f05a15e92dbbd8b0603b1b4b65a9e81411798e52
yuyuc <yuyuchia7423@gmail.com>
2022-06-23 02:38:15 +0800

    :construction: Work in progress.

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,14 @@
 <script setup lang="ts">
-import HelloWorld from './components/HelloWorld.vue'
-
 
 </script>
 
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Hello Vue 3 + TypeScript + Vite" />
+  <div>
+    <h1>Hello World</h1>
+    <p>This is a simple example of a Vue.js component.</p>
+  </div>
 </template>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
 </style>
