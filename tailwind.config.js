3065e6f2798ff7a5d2207f981fd256bb6eb339ca
yuyuc <yuyuchia7423@gmail.com>
2022-06-23 02:30:32 +0800

    :tada: Begin a project.

---
+++ tailwind.config.js
@@ -0,0 +1,44 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./index.html",
+    "./src/**/*.{vue,js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        'primary': '#2B2E4A',
+        'secondary': '#ff9b1a',
+        'sec-text': '#444444',
+        'success': '#4caf50',
+        'info': '#2196f3',
+        'warning': '#ff9800',
+        'danger': '#f44336',
+        'light': '#fafafa',
+        'dark': '#212121',
+        'white': '#ffffff',
+        'black': '#000000',
+        'code': '#24292e',
+      },
+      keyframes: {
+        wave: {
+          '0%': { transform: 'rotate(0.0deg)' },
+          '10%': { transform: 'rotate(14deg)' },
+          '20%': { transform: 'rotate(-8deg)' },
+          '30%': { transform: 'rotate(14deg)' },
+          '40%': { transform: 'rotate(-4deg)' },
+          '50%': { transform: 'rotate(10.0deg)' },
+          '60%': { transform: 'rotate(0.0deg)' },
+          '100%': { transform: 'rotate(0.0deg)' },
+        },
+      },
+      animation: {
+        'waving-hand': 'wave 3.5s ease 1s infinite',
+      },
+    },
+  },
+  plugins: [
+    require('@tailwindcss/line-clamp'),
+    require('@tailwindcss/aspect-ratio'),
+  ],
+}
