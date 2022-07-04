fcf78b265e0d4736878d83372aa6972dff0a75fe
Chia1104 <yuyuchia7423@gmail.com>
2022-07-04 18:21:41 +0800

    :ambulance: Critical hotfix.

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,7 +1,7 @@
 import { initializeApp, FirebaseOptions, getApp } from "firebase/app";
 import { getStorage } from "firebase/storage";
 import { getFirestore } from "firebase/firestore";
-import { getAuth } from "@firebase/auth";
+import { getAuth } from "firebase/auth";
 
 const firebaseConfig: FirebaseOptions = {
     apiKey: import.meta.env.VITE_API_KEY,
